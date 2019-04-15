e8ecd5bab166998fce25fbd33deb8303af9f2152
Alexmartineztapia <Alexmartineztapia@users.noreply.github.com>
2019-04-15 15:55:15 +0200

    comunicació serie basica

---
+++ comunicació serie basica/programa_04/programa_04.ino
@@ -0,0 +1,38 @@
+    
+/*****************************************************************
+**                                                              **
+**                        Títol: Caixer                         **
+**                                                              **
+**                                                              **
+**                                                              **
+*****************************************************************/
+//****************************INCLUDE*****************************
+
+
+//***************************VARIABLES****************************
+
+//*****************************SETUP******************************
+
+void setup() {                
+  
+Serial.begin(9600);
+Serial.println("Escull el numero de l'operació que vols realitzar?");
+Serial.println("  1. Comprovar numero de tarjeta de credit");
+Serial.println("  2. Comprovar numero de compte bancari");
+Serial.println("  3. Buscar un digit perdut de tarjeta de credit");
+   
+}
+
+
+
+
+
+//******************************LOOP******************************
+
+void loop() {                
+
+
+
+}
+
+//****************************FUNCIONS****************************
