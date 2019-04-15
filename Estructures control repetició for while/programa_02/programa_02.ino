badbb17027dfc3ba30c14a63e7b2c6af8b66136f
Alexmartineztapia <Alexmartineztapia@users.noreply.github.com>
2019-04-15 15:46:34 +0200

    for while

---
+++ Estructures control repetició for while/programa_02/programa_02.ino
@@ -0,0 +1,37 @@
+    
+/*****************************************************************
+**                                                              **
+**                        Títol: comptador while                **
+**                                                              **
+**                                                              **
+**                                                              **
+*****************************************************************/
+//****************************INCLUDE*****************************
+
+
+//***************************VARIABLES****************************
+int comptar = 11;
+int i = 0;
+//*****************************SETUP******************************
+void setup()               // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+  Serial.print("Ara comptare de 0 a ");
+  Serial.println(comptar);  
+  while(i <= comptar)
+  {
+   
+    Serial.print(i);
+     if (i<comptar)
+    Serial.print("-");
+    i++;
+  } 
+}
+//******************************LOOP******************************
+
+
+void loop()   // we need this to be here even though its empty
+{
+}
+
+//****************************FUNCIONS****************************
